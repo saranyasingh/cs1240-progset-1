9d68e51babd9540e9565c5b1267083cbc2dcc873
Saranya Singh <saranyasingh926@gmail.com>
2026-02-14 16:52:35 -0500

    v1, not tested

--- randmst.py
+++ randmst.py
@@ -1,6 +1,48 @@
 import random 
 import math
 
+class MinHeap:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        self.data.append(item)
+        i = len(self.data) - 1
+
+        # bubble up 
+        while i > 0:
+            parent = (i - 1) // 2
+            if self.data[i] < self.data[parent]:
+                self.data[i], self.data[parent] = self.data[parent], self.data[i]
+                i = parent
+            else:
+                break
+
+    def pop(self):
+        self.data[0], self.data[len(self.data) - 1] = self.data[len(self.data) - 1], self.data[0]
+        item = self.data.pop()
+        i = 0
+        n = len(self.data)
+
+        # bubble down 
+        while True:
+            left = 2*i + 1
+            right = 2*i + 2
+            smallest = i
+
+            if left < n and self.data[left] < self.data[smallest]:
+                smallest = left
+            if right < n and self.data[right] < self.data[smallest]:
+                smallest = right
+
+            if smallest == i:
+                break
+
+            self.data[i], self.data[smallest] = self.data[smallest], self.data[i]
+            i = smallest
+        return item
+
+
 def complete_weighted_graph(n):
     adj = {p: [] for p in range(n)}
 
@@ -82,5 +124,30 @@ def unit_hypercube_graph(n):
 
     return adj
 
-def prim_alg(G):
-     
+def prim_mst(G):
+    start = G[0]
+    visited = {start}
+    heap = MinHeap()
+
+    mst_edges = []
+    total_weight = 0
+
+    # Add initial edges for the start vertex
+    for neighbor, weight in G[start]:
+        heap.push((weight, start, neighbor))
+
+    while not heap.is_empty() and len(visited) < len(G):
+        weight, u, v = heap.pop()
+
+        if v in visited:
+            continue
+
+        visited.add(v)
+        mst_edges.append((u, v, weight))
+        total_weight += weight
+
+        for neighbor, w in G[v]:
+            if neighbor not in visited:
+                heap.push((w, v, neighbor))
+
+    return mst_edges, total_weight
